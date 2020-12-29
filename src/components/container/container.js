48b7c2f114c800560da5119b3c16302f29dd3c07
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-28 16:08:27 -0800

    setting up container component and some of its relevant css

---
+++ src/components/container/container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './container.css';
+
+const Container = () => {
+    return (
+        <div class="present">
+    <div class="lid">
+        <span></span>
+    </div>
+    <div class="content">
+        <div class="square" />
+    </div>
+    <div class="box">
+        <span></span>
+        <span></span>
+    </div>
+</div>
+    )
+}
+
+export default Container;
